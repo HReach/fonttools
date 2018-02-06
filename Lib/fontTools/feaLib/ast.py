e46c1b11a3243f69d028f0f33e7bacfadd1b0fca
Miguel Sousa <msousa@adobe.com>
2018-02-05 22:45:56 -0800

    [feaLib] Simplify builder.add_featureName()

--- Lib/fontTools/feaLib/ast.py
+++ Lib/fontTools/feaLib/ast.py
@@ -1110,7 +1110,7 @@ class NameRecord(Statement):
 class FeatureNameStatement(NameRecord):
     def build(self, builder):
         NameRecord.build(self, builder)
-        builder.add_featureName(self.location, self.nameID)
+        builder.add_featureName(self.nameID)
 
     def asFea(self, indent=""):
         if self.nameID == "size":
